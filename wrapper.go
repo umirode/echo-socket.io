e38e1ce754ac859434e03f409db4a01c36265f0e
mr.droiderm <mr.droiderm@gmail.com>
2019-03-12 13:52:52 +0300

    add docblock, add interfaces

--- wrapper.go
+++ wrapper.go
@@ -5,17 +5,42 @@ import (
 	"github.com/googollee/go-engine.io"
 	"github.com/googollee/go-socket.io"
 	"github.com/labstack/echo"
+	"io"
+	"net/http"
 )
 
-type SocketIOOnConnectHandler func(echo.Context, socketio.Conn) error
-type SocketIOOnDisconnectHandler func(echo.Context, socketio.Conn, string)
-type SocketIOOnErrorHandler func(echo.Context, error)
-type SocketIOOnEventHandler func(echo.Context, socketio.Conn, string)
+/**
+Socket.io server interface
+*/
+type ISocketIO interface {
+	http.Handler
+	io.Closer
 
-type Wrapper struct {
-	Server *socketio.Server
+	Serve() error
+
+	OnConnect(nsp string, f func(socketio.Conn) error)
+	OnDisconnect(nsp string, f func(socketio.Conn, string))
+	OnError(nsp string, f func(error))
+	OnEvent(nsp, event string, f interface{})
 }
 
+/**
+Socket.io wrapper interface
+*/
+type ISocketIOWrapper interface {
+	OnConnect(context echo.Context, nsp string, f func(echo.Context, socketio.Conn) error)
+	OnDisconnect(context echo.Context, nsp string, f func(echo.Context, socketio.Conn, string))
+	OnError(context echo.Context, nsp string, f func(echo.Context, error))
+	OnEvent(context echo.Context, nsp, event string, f func(echo.Context, socketio.Conn, string))
+}
+
+type Wrapper struct {
+	Server ISocketIO
+}
+
+/**
+Create wrapper and Socket.io server
+*/
 func NewWrapper(options *engineio.Options) (*Wrapper, error) {
 	server, err := socketio.NewServer(options)
 	if err != nil {
@@ -27,7 +52,10 @@ func NewWrapper(options *engineio.Options) (*Wrapper, error) {
 	}, nil
 }
 
-func NewWrapperWithServer(server *socketio.Server) (*Wrapper, error) {
+/**
+Create wrapper with exists Socket.io server
+*/
+func NewWrapperWithServer(server ISocketIO) (*Wrapper, error) {
 	if server == nil {
 		return nil, errors.New("socket.io server can not be nil")
 	}
@@ -37,25 +65,37 @@ func NewWrapperWithServer(server *socketio.Server) (*Wrapper, error) {
 	}, nil
 }
 
-func (s *Wrapper) OnConnect(context echo.Context, nsp string, f SocketIOOnConnectHandler) {
+/**
+On Socket.io client connect
+*/
+func (s *Wrapper) OnConnect(context echo.Context, nsp string, f func(echo.Context, socketio.Conn) error) {
 	s.Server.OnConnect(nsp, func(conn socketio.Conn) error {
 		return f(context, conn)
 	})
 }
 
-func (s *Wrapper) OnDisconnect(context echo.Context, nsp string, f SocketIOOnDisconnectHandler) {
+/**
+On Socket.io client disconnect
+*/
+func (s *Wrapper) OnDisconnect(context echo.Context, nsp string, f func(echo.Context, socketio.Conn, string)) {
 	s.Server.OnDisconnect(nsp, func(conn socketio.Conn, msg string) {
 		f(context, conn, msg)
 	})
 }
 
-func (s *Wrapper) OnError(context echo.Context, nsp string, f SocketIOOnErrorHandler) {
+/**
+On Socket.io error
+*/
+func (s *Wrapper) OnError(context echo.Context, nsp string, f func(echo.Context, error)) {
 	s.Server.OnError(nsp, func(e error) {
 		f(context, e)
 	})
 }
 
-func (s *Wrapper) OnEvent(context echo.Context, nsp, event string, f SocketIOOnEventHandler) {
+/**
+On Socket.io event from client
+*/
+func (s *Wrapper) OnEvent(context echo.Context, nsp, event string, f func(echo.Context, socketio.Conn, string)) {
 	s.Server.OnEvent(nsp, event, func(conn socketio.Conn, msg string) {
 		f(context, conn, msg)
 	})
